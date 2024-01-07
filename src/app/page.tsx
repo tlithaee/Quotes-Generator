0531e2c09fc7f8fc361f7bd1847ada9c9477e150
tlithaee <syarifah.talithae@gmail.com>
2024-01-07 09:27:18 +0700

    modified:   src/app/globals.css 	modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,23 @@ export default function Home() {
   const handleAPI = async () => {
     try {
       const response = await axios.get('https://api.quotable.io/quotes/random');
-      console.log(response.data);
+      // console.log(response.data);
+      console.log(`Content: ${response.data[0]?.content}`)
       return response.data;
     } catch (error) {
       console.log(error);
     }
   };
 
-  // handleAPI();
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-blue-600/20">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-
+        <h1>
+     
+        </h1>
       </div>
 
-      <div className="card mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <button onClick={handleAPI}>Make API Call</button>
       </div>
     </main>
