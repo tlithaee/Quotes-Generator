4e838070e2f180745ff4fc2dbaf8107c7644ae3a
tlithaee <syarifah.talithae@gmail.com>
2024-01-07 10:02:46 +0700

    modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 'use client'
 
 import axios from 'axios';
+import { useState } from 'react';
 
 export default function Home() {
+  const [quote, setQuote] = useState('');
 
   const handleAPI = async () => {
     try {
       const response = await axios.get('https://api.quotable.io/quotes/random');
-      // console.log(response.data);
-      console.log(`Content: ${response.data[0]?.content}`)
-      return response.data;
+      console.log(`Content: ${response.data[0]?.content}`);
+      setQuote(response.data[0]?.content);
+      // return response.data;
     } catch (error) {
       console.log(error);
     }
@@ -19,12 +21,12 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-blue-600/20">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1>
-     
+          {quote}
         </h1>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <button onClick={handleAPI}>Make API Call</button>
+        <button className="py-30" onClick={handleAPI}>Make API Call</button>
       </div>
     </main>
   )
